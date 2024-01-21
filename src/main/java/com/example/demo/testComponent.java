5b8234427f4d7e58d20745841d81bea00cb4d334
ParkSungCheol <skfo8gmlakd@naver.com>
2024-01-21 14:31:37 +0900

    GIT ACTIONS TEST

---
+++ src/main/java/com/example/demo/testComponent.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testComponent {
+
+    @GetMapping("/")
+    public String test4() {
+        return "test4 is running!@#$";
+    }
+}
